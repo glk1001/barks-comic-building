25e23a2e38127e703ae4022d640fc509cb742762
glk1001 <gregg.kay@gmail.com>
2026-02-20 10:23:09 +1100

    Refactored some duplicated panels code.

--- barks-cmds/show-panel-bounds.py
+++ barks-cmds/show-panel-bounds.py
@@ -1,16 +1,17 @@
-import json
 from pathlib import Path
 
 import cv2 as cv
 import typer
+from barks_fantagraphics.barks_titles import BARKS_TITLE_DICT
 from barks_fantagraphics.comics_consts import PNG_FILE_EXT, RESTORABLE_PAGE_TYPES
 from barks_fantagraphics.comics_database import ComicsDatabase
-from barks_fantagraphics.comics_utils import get_abbrev_path
+from barks_fantagraphics.comics_helpers import draw_panel_bounds_on_image
+from barks_fantagraphics.panel_boxes import TitlePanelBoxes, check_page_panel_boxes
 from comic_utils.common_typer_options import LogLevelArg, TitleArg
 from comic_utils.cv_image_utils import get_bw_image_from_alpha
 from loguru import logger
 from loguru_config import LoguruConfig
-from PIL import Image, ImageDraw
+from PIL import Image
 
 APP_LOGGING_NAME = "span"
 
@@ -23,64 +24,25 @@ def show_panel_bounds(comics_database: ComicsDatabase, title: str, out_dir: Path
     out_dir.mkdir(parents=True, exist_ok=True)
     comic = comics_database.get_comic_book(title)
     svg_files = comic.get_srce_restored_svg_story_files(RESTORABLE_PAGE_TYPES)
-    panel_segments_files = comic.get_srce_panel_segments_files(RESTORABLE_PAGE_TYPES)
+    title_pages_panel_boxes = TitlePanelBoxes(comics_database).get_page_panel_boxes(
+        BARKS_TITLE_DICT[title]
+    )
 
-    for svg_file, panel_segments_file in zip(svg_files, panel_segments_files, strict=True):
+    for svg_file in svg_files:
+        fanta_page = svg_file.stem
+
         png_file = Path(str(svg_file) + PNG_FILE_EXT)
-        bounds_img_file = out_dir / (Path(svg_file).stem + "-with-bounds.png")
+        bounds_img_file = out_dir / (fanta_page + "-with-bounds.png")
         bw_image = get_bw_image_from_alpha(png_file)
         pil_image = Image.fromarray(cv.merge([bw_image, bw_image, bw_image])).convert("RGBA")
-        if not write_bounds_to_pil_image(pil_image, panel_segments_file):
-            msg = "There were process errors."
-            raise RuntimeError(msg)
+
+        page_panel_boxes = title_pages_panel_boxes.pages[fanta_page]
+        check_page_panel_boxes(pil_image.size, page_panel_boxes)
+        draw_panel_bounds_on_image(pil_image, page_panel_boxes)
+
         pil_image.save(bounds_img_file)
 
     logger.info(f'Finished generating panel bounds images for "{title}" to directory "{out_dir}".')
-
-
-def write_bounds_to_pil_image(
-    pil_image: Image.Image, panel_segments_file: Path, include_overall_bound: bool = True
-) -> bool:
-    logger.info(
-        f'Writing bounds using segments info file "{get_abbrev_path(panel_segments_file)}"...'
-    )
-
-    if not panel_segments_file.is_file():
-        logger.error(f'Could not find panel segments file "{panel_segments_file}".')
-        return False
-
-    logger.info(f'Loading panel segments file "{get_abbrev_path(panel_segments_file)}".')
-    with panel_segments_file.open("r") as f:
-        panel_segment_info = json.load(f)
-
-    if pil_image.size[0] != panel_segment_info["size"][0]:
-        msg = (
-            f"Image size[0] {pil_image.size[0]}"
-            f" does not match panel segment info size[0] {panel_segment_info['size'][0]}."
-        )
-        raise RuntimeError(msg)
-    if pil_image.size[1] != panel_segment_info["size"][1]:
-        msg = (
-            f"Image size[1] {pil_image.size[1]}"
-            f" does not match panel segment info size[1] {panel_segment_info['size'][1]}."
-        )
-        raise RuntimeError(msg)
-
-    draw = ImageDraw.Draw(pil_image)
-    for box in panel_segment_info["panels"]:
-        x0 = box[0]
-        y0 = box[1]
-        w = box[2]
-        h = box[3]
-        x1 = x0 + (w - 1)
-        y1 = y0 + (h - 1)
-        draw.rectangle([x0, y0, x1, y1], outline="green", width=10)
-
-    if include_overall_bound:
-        x0, y0, x1, y1 = panel_segment_info["overall_bounds"]
-        draw.rectangle([x0, y0, x1, y1], outline="red", width=2)
-
-    return True
 
 
 app = typer.Typer()
